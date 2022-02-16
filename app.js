a8c486cfae78f87f4e46c7fc0d473595230947fb
11ipsita <61733847+11ipsita@users.noreply.github.com>
2022-02-16 21:49:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,14 +5,8 @@ const port = 8080;
 
 var route = require('./route');
 
-app.use('/public', express.static('public'));  //public folder 
 //app.use('/', route);
 app.use('/users', route); // For databse
-
-/*app.set('view engine', 'ejs');   //set ejs view engine....
-app.set('views', path.join(__dirname, './resource'));*/
-
-//app.set('view engine', 'pug'); 
 
 
 
